4cc1fa2c42daf26c144047c6a5c154c03f45a230
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-13 11:54:37 +0200

    Test inequality

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -7,8 +7,15 @@ import source.shapes as shapes
 def my_rectangle():
     return shapes.Rectangle(20, 10)
 
+@pytest.fixture
+def another_rectangle():
+    return shapes.Rectangle(30, 15)
+
 def test_area(my_rectangle):
     assert my_rectangle.area() == 10 * 20
 
 def test_perimeter(my_rectangle):
     assert my_rectangle.perimeter() == 2*20 + 2*10
+
+def test_not_eq(my_rectangle, another_rectangle):
+    assert my_rectangle != another_rectangle
