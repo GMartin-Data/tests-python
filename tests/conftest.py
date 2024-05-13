266e2b83fb83a52e83c9e0baeafb626a70b07f82
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-13 13:55:58 +0200

    Implement school.py and related fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 import source.shapes as shapes
+import source.school as school
 
 
 @pytest.fixture
@@ -12,3 +13,16 @@ def rectangle_factory():
 @pytest.fixture
 def my_circle():
     return shapes.Circle(10)
+
+@pytest.fixture
+def student_factory():
+    def create_student(student_id, name) -> school.Student:
+        return school.Student(student_id=student_id, name=name)
+    return create_student
+
+@pytest.fixture
+def course_factory():
+    def create_course(course_id, title) -> school.Course:
+        return school.Course(course_id=course_id, title=title)
+    return create_course
+
