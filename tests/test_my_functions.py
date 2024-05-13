9eb7a304f9c6fe3bd571f0d94ecb6f81bf24ada0
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-13 10:10:26 +0200

    Package all and implement some tests

---
+++ tests/test_my_functions.py
@@ -0,0 +1,17 @@
+import pytest
+
+import source.my_functions as smf
+
+
+def test_add_pass():
+    assert smf.add(1, 4) == 5
+
+def test_add_fail():
+    assert not smf.add(1, 2) == 4    
+
+def test_divide_pass():
+    assert smf.divide(6, 2) == 3
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        smf.divide(number_one=10, number_two=0)
