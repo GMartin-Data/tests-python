d5a777fbc86019333a7e104051ecbb58ac52d1fe
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-13 11:24:46 +0200

    Finished TestCircle

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,11 +1,16 @@
+import math
+
 import pytest
 
 import source.shapes as shapes
 
 
 class TestCircle:
-    def setup_method(self, method):
-        print(f"Setting up {method}")
+    def setup_method(self):
+        self.circle = shapes.Circle(10)
 
-    def test_one(self):
-        assert True
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_perimeter(self):
+        assert self.circle.perimeter() == 2 * math.pi * self.circle.radius
