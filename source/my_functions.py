7898845b37a5ae23fb7fe91cee4b9b551a1f0d09
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-13 10:15:44 +0200

    Implement multiply function

--- source/my_functions.py
+++ source/my_functions.py
@@ -3,3 +3,6 @@ def add(number_one, number_two):
 
 def divide(number_one, number_two):
     return number_one / number_two
+
+def multiply(factor_one, factor_two):
+    return factor_one * factor_two
